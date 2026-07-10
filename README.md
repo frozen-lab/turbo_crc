403a8b4958fe894bd98798986172b219abaf77d4
Arctic Byte <77727680+adityamotale@users.noreply.github.com>
2026-07-10 11:42:00 +0530

    (rs) updated `Cargo.toml` and `README.md` for v0.0.0 release

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # TurboCrc
+
+Hardware accelerated implementation of CRC32C for computing 32-bit cyclic redundency check (CRC)
--- turbo_crc/src/lib.rs
+++ turbo_crc/src/lib.rs
@@ -1,6 +1,6 @@
-//! Hardware accelerated implementation of CRC32C for computing 32-bit Cyclic Redundency Check (CRC)
+//! Hardware accelerated implementation of CRC32C for computing 32-bit cyclic redundency check (CRC)
 
-/// Hardware accelerated implementation of CRC32C for computing 32-bit Cyclic Redundency Check (CRC)
+/// Hardware accelerated implementation of CRC32C for computing 32-bit cyclic redundency check (CRC)
 ///
 /// ## Example
 ///
