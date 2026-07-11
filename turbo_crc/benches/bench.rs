4d919bdeb0d70a1e8a693bcade60cf098e530f62
Arctic Byte <77727680+adityamotale@users.noreply.github.com>
2026-07-11 12:10:38 +0530

    (rs) updated module docs to include bench results

--- turbo_crc/benches/bench.rs
+++ turbo_crc/benches/bench.rs
@@ -1,9 +1,12 @@
+/// Benchmarks to observe througput of crc
+/// Run using: `taskset -c 1 cargo bench --bench bench --profile bench`
+
 const KB: usize = 0x400;
 const MB: usize = KB * KB;
 
-#[divan::bench(args = [64 * KB, 256 * KB, 1 * MB, 16 * MB, 64 * MB, 128 * MB])]
+#[divan::bench(args = [64 * KB, 256 * KB, 1 * MB, 16 * MB, 64 * MB])]
 fn crc_throughput(bencher: divan::Bencher, size: usize) {
-    let data = vec![0x0Au8; size];
+    let data = vec![(size & 0xFF) as u8; size];
 
     bencher
         .counter(divan::counter::BytesCount::new(size))
--- turbo_crc/src/lib.rs
+++ turbo_crc/src/lib.rs
@@ -1,5 +1,18 @@
 //! Hardware accelerated implementation of CRC32C for computing 32-bit cyclic redundency check (CRC)
 //!
+//! ## Benchmark
+//!
+//! Observed throughput across multiple benchmark runs,
+//!
+//! | Buffer Size | Throughput |
+//! |:------------|:-----------|
+//! | 64 KiB      | 523 MB/s   |
+//! | 256 KiB     | 522 MB/s   |
+//! | 1 MiB       | 521 MB/s   |
+//! | 16 MiB      | 513 MB/s   |
+//!
+//! TL;DR: Sustains ~0.5 GiB/s across buffers from 64 KiB to 64 MiB
+//!
 //! ## Example
 //!
 //! ```
