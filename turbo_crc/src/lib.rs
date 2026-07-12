377daf8e8494ece833daa123a88fc330197dc6a3
Arctic Byte <77727680+adityamotale@users.noreply.github.com>
2026-07-12 15:41:47 +0530

    (rs) impl of `crc32c` using ArmV8 `crc` instruction

--- turbo_crc/src/lib.rs
+++ turbo_crc/src/lib.rs
@@ -23,7 +23,7 @@
 //! assert_eq!(turbo_crc::crc32c(standard_vector), 0x6087809A);
 //! ```
 
-/// Compute a 32-bit crc for a given data buffer
+/// Compute a hardware accelerated 32-bit crc for a given data buffer using `sse4.2` ISA
 ///
 /// ## Example
 ///
@@ -47,6 +47,39 @@ pub fn crc32c(buffer: &[u8]) -> u32 {
         unsafe {
             let qword = core::ptr::read_unaligned(ptr as *const u64);
             crc = core::arch::x86_64::_mm_crc32_u64(crc, qword);
+
+            ptr = ptr.add(8);
+            len -= 8;
+        }
+    }
+
+    (!crc) as u32
+}
+
+/// Compute a hardware accelerated 32-bit crc for a given data buffer using ArmV8 `crc` instruction
+///
+/// ## Example
+///
+/// ```
+/// extern crate turbo_crc;
+///
+/// let standard_vector = b"12345678";
+/// assert_eq!(turbo_crc::crc32c(standard_vector), 0x6087809A);
+/// ```
+#[inline(always)]
+#[cfg(target_arch = "aarch64")]
+pub fn crc32c(buffer: &[u8]) -> u32 {
+    // sanity check
+    debug_assert!(buffer.len() & 7 == 0, "Input buffer must be 8 bytes aligned");
+
+    let mut crc = (!0u32) as u64;
+    let mut len = buffer.len();
+    let mut ptr = buffer.as_ptr();
+
+    while len > 0 {
+        unsafe {
+            let qword = core::ptr::read_unaligned(ptr as *const u64);
+            crc = core::arch::aarch64::__crc32cd(crc, qword);
 
             ptr = ptr.add(8);
             len -= 8;
